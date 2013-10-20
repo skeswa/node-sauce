32b2cb76e02299abdb24db52723f44f5520c6e65
Sandile Keswa <sandile.keswa@gmail.com>
2013-10-20 19:31:42 -0400

    The github call now works fine.

--- test/server.js
+++ test/server.js
@@ -29,27 +29,35 @@ app.use(sauce.bind(app).configure({
 }).express());
 
 // Test routes
-app.get("/google", function (req, res) {
+app.get("/google", function(req, res) {
     if (!req.sauce.apis.google.authed()) {
         req.sauce.apis.google.auth("/google");
     } else {
         var client = req.sauce.apis.google.client();
-        var userRequest = client.get("oauth2/v1/userinfo").success(function (user) {
+        var userRequest = client.get("oauth2/v1/userinfo").success(function(user) {
             res.json(200, user);
-        }).failure(function (err) {
+        }).failure(function(err) {
             res.send(500, user);
         });
         userRequest.call();
     }
 });
-app.get("/github", function (req, res) {
+app.get("/github", function(req, res) {
     if (!req.sauce.apis.github.authed()) {
         req.sauce.apis.github.auth("/github");
     } else {
-        res.send("WOOP - GITHUB SON.");
+        var client = req.sauce.apis.github.client();
+        var userRequest = client.get("user").success(function(user) {
+            res.json(200, user);
+        }).failure(function(err, resp, bod) {
+            res.send(500, err);
+            console.log(resp);
+            console.log(bod);
+        });
+        userRequest.call();
     }
 });
 
 app.listen(7373, function() {
-	console.log("Server started on port " + 7373);
+    console.log("Server started on port " + 7373);
 });
