9f8a365a0b86bede665918abc8d10fc55ee9c205
Sandile Keswa <sandile.keswa@gmail.com>
2013-10-22 23:03:30 -0400

    tested my google api impl, and auth with callbacks works now

--- test/testServer1.js
+++ test/testServer1.js
@@ -1,4 +1,18 @@
-var fakeGoogleApi = new require("../lib/apis/oauth2api.js")("fakeGoogle", {}, {
+var express = require("express");
+var app = express();
+
+app.use(express.cookieParser());
+var MemStore = express.session.MemoryStore;
+app.use(express.session({
+    secret: 'secret_key',
+    store: MemStore({
+        reapInterval: 60000 * 10
+    })
+}));
+app.use(express.bodyParser());
+
+var Api = require("../lib/api/type/oauth2.js");
+var fakeGoogleApi = new Api("google", {}, {
     clientId: "1073639428455-4i31qgcbhon7dvstd9r6efeo7rhcsedl.apps.googleusercontent.com",
     clientSecret: "TdJQNp1INvQHdCINUiQbR6PZ",
     scopes: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/gcm_for_chrome",
@@ -13,4 +27,23 @@ var fakeGoogleApi = new require("../lib/apis/oauth2api.js")("fakeGoogle", {}, {
     clientConfig: {
         authorizationHeaderPrefix: "OAuth"
     }
+});
+console.log(fakeGoogleApi);
+
+app.get("/test", function(req, res) {
+    if (!fakeGoogleApi.authed(req.session)) {
+        fakeGoogleApi.auth(app, req, res, "/test").call(function (err, token) {
+            if (err) {
+                console.log("OH NO!");
+            } else {
+                console.log("yeah baby: " + token);
+            }
+        });
+    } else {
+        res.send(200);
+    }
+});
+
+app.listen(7373, function() {
+    console.log("Server started on port " + 7373);
 });
